5e5f7e0310d2b4239dc3da51f6aba512b9a10c0e
ralf789 <51427526+ralf789@users.noreply.github.com>
2019-10-18 09:54:09 -0400

    Add files via upload

---
+++ Jeux de devinette/Jeux de devinette/Form1.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.cmdDevinez = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtChiffre = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'cmdDevinez
+        '
+        Me.cmdDevinez.Font = New System.Drawing.Font("Modern No. 20", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdDevinez.Location = New System.Drawing.Point(105, 198)
+        Me.cmdDevinez.Name = "cmdDevinez"
+        Me.cmdDevinez.Size = New System.Drawing.Size(103, 41)
+        Me.cmdDevinez.TabIndex = 0
+        Me.cmdDevinez.Text = "Devinez"
+        Me.cmdDevinez.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Modern No. 20", 16.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.OrangeRed
+        Me.Label1.Location = New System.Drawing.Point(54, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(208, 30)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Jeu de devinette"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Monotype Corsiva", 10.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Red
+        Me.Label2.Location = New System.Drawing.Point(55, 39)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(199, 20)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Entrez un chiffre de 1 à 73"
+        '
+        'txtChiffre
+        '
+        Me.txtChiffre.Location = New System.Drawing.Point(105, 115)
+        Me.txtChiffre.Name = "txtChiffre"
+        Me.txtChiffre.Size = New System.Drawing.Size(100, 22)
+        Me.txtChiffre.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(306, 288)
+        Me.Controls.Add(Me.txtChiffre)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cmdDevinez)
+        Me.Name = "Form1"
+        Me.Text = "Es-tu chanceux?"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents cmdDevinez As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtChiffre As System.Windows.Forms.TextBox
+
+End Class
---
+++ Jeux de devinette/Jeux de devinette/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Dim num As Integer
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Randomize()
+        num = Int((Rnd() * 73) + 1)
+    End Sub
+    Private Sub cmdDevinez_Click(sender As Object, e As EventArgs) Handles cmdDevinez.Click
+        If IsNumeric(txtChiffre.Text) = False Then
+            MsgBox("Veuillez entrez un chiffre de 1 à 73")
+            Exit Sub
+        End If
+        If Val(txtChiffre.Text) < 1 Or Val(txtChiffre.Text) > 73 Then
+            MsgBox("veuillez entrez un chiffre entre 1 et 73")
+            Exit Sub
+        End If
+        If Val(txtChiffre.Text) > num Then
+            MsgBox("Ce chiffre est trop haut")
+        ElseIf Val(txtChiffre.Text) < num Then
+            MsgBox("Ce chiffre est trop bas")
+        Else
+            MsgBox("Ce chiffre est BON! Bravo!")
+            End
+        End If
+    End Sub
+End Class
